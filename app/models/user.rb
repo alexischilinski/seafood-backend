e427d2400d5df9bd03342741e15ed9b284487fab
alexiscait142 <chilinski.a@gmail.com>
2020-01-23 13:10:45 -0700

    final touches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  belongs_to :region
+  belongs_to :region, dependent: :destroy
   has_many :user_fishes
   has_many :fishes, through: :user_fishes
 end
